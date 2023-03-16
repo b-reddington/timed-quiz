2096acc0691050a839619137ec6fdc91729cf2e6
b-reddington <brandon.reddington98@gmail.com>
2023-03-15 22:17:09 -0700

    Added if statements and a for loop to generate buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,56 +2,78 @@ let container = document.getElementById("quiz-card");
 let startButton = document.createElement("button");
 startButton.textContent = "Start Quiz"; // Add "Start Quiz" as the inner text of the button
 container.appendChild(startButton); // append the start button into the container element
-let answerButton = document.createElement("button");
+let questions = [
+    {
+        question: "Commonly used data types DO NOT Include:",
+        answers: ["strings", "booleans", "alerts", "numbers"],
+        correctAnswer: "alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ______",
+        answers: ["quotes", "curly brackets", "parantheses", "square brackets"],
+        correctAnswer: "curly brackets"
+    },
+    {
+        question: "Arrays in JavaSript can be used to store _______",
+        answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correctAnswer: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables",
+        answers: ["commas", "curly brackets", "quotes", "parentheses"],
+        correctAnswer: "quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        correctAnswer: "console.log"
+    },
+];
+let score = 0;
+
+
+function generateButtons(score) {
+    for (let i = 0; i < questions[0].answers.length; i++) {
+    let answerButton = document.createElement("button");
+    answerButton.textContent = questions[score].answers[i];
+    answerButton.setAttribute("type", "button");
+    answerButton.setAttribute("id", questions[score].answers[i]);
+    container.appendChild(answerButton);
+    answerButton.addEventListener("click", function() {
+        if (this.id === questions[score].correctAnswer)
+        score++;
+        console.log("score: " + score + "\ni = " + i + "\n" + answerButton.id + "\nquestions.correctAnswer = " + questions[score].correctAnswer);
+    })
+    }
+    
+}
 
 function init() {
     let questionEl = document.createElement("h2"); // Creates an h2 to append the questions to
     container.appendChild(questionEl); // Appends the questionEl to the container element
-    let correct = 0;
-    let questions = [
-        {
-            question: "Commonly used data types DO NOT Include:",
-            answers: ["strings", "booleans", "alerts", "numbers"],
-            correctAnswer: "alerts"
-        },
-        {
-            question: "The condition in an if/else statement is enclosed within ______",
-            answers: ["quotes", "curly brackets", "parantheses", "square brackets"],
-            correctAnswer: "curly brackets"
-        },
-        {
-            question: "Arrays in JavaSript can be used to store _______",
-            answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-            correctAnswer: "all of the above"
-        },
-        {
-            question: "String values must be enclosed within _____ when being assigned to variables",
-            answers: ["commas", "curly brackets", "quotes", "parentheses"],
-            correctAnswer: "quotes"
-        },
-        {
-            question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-            answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-            correctAnswer: "console.log"
+    
+        if (score === 0) {
+        questionEl.textContent = questions[score].question;
+        generateButtons(score);
         }
-    ];
-    //create a for loop to avoid repetitive code
-    for (let q = 0; q < questions.length; q++) {
-        if (correct === 0) {
-        questionEl.textContent = questions[q].question;
+        if (score === 1) {
+        questionEl.textContent = questions[score].question;
+        generateButtons(score);
+        }
+        if (score === 2) {
+        questionEl.textContent = questions[score].question;
+        generateButtons(score);
+        }
+        if (score === 3) {
+        questionEl.textContent = questions[score].question;
+        generateButtons(score);
+        }
+        if (score === 4) {
+        questionEl.textContent = questions[score].question;
+        generateButtons(score);
         }
     }
-    // container.appendChild(questions[q].answers);
 
-    // questionEl.textContent = questions[0].question;
-    // // answerButtonOne.textContent = questions[0];
-    // answerButton.textContent = "Answer 2";
-    // answerButton.textContent = "Answer 3";
-    // answerButton.textContent = "Answer 4";
-
-
-
-}
 
 startButton.addEventListener("click", function () {
     startButton.remove(); // When start button is clicked, remove the start button element
